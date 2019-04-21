4f3a1e8536b4320b39afa7b4720cb27cbcf43373
gaomiaofei <gaomiaofei@gaomiaofeis-MacBook-Pro.local>
2019-04-21 23:50:24 +0800

    merge

--- src/component/videoItem/index.js
+++ src/component/videoItem/index.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-
+import 'video-react/dist/video-react.css';
+import { Player } from 'video-react';
 class VideoItem extends Component{
     constructor(props){
         super(props);
         console.log(props)
     }
-//     componentDidMount() {
-// 　　    const { classItem } = this.props;
-//         console.log(this.props)
-// 　  }
+    componentDidMount() {
+　　    const { classItem } = this.props;
+        console.log(this.props)
+　  }
 　　render() {
 　　    return (
 　　　　　   <div className="nav">
-                第{this.props.classItem}节课！
+                {/* 第{this.props.classItem}节课！ */}
+                <Player>
+                    <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
+                </Player>
             </div>
 　　　  )
     }
